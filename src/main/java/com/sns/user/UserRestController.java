8e66aed550341d5a9af9770a8438e2a41d74ef2e
학원 <testtest@gmail.com>
2023-10-30 21:43:36 +0900

    10/30

---
+++ src/main/java/com/sns/user/UserRestController.java
@@ -0,0 +1,13 @@
+package com.sns.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserRestController {
+
+	
+	//@GetMapping("/")
+	//public 
+}
